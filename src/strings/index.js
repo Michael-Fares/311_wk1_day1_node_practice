b9486f1266628ca5a0c1d66d892b884a5b4490bf
Michael-Fares <mjfares@gmail.com>
2021-06-28 09:35:58 -0500

    strings done

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
+  let results = [];
   // write code for strings.pairs
-
+  for(let i=2; i<str.length+2; i+=2) {
+   results.push(str.slice(i-2,i))
+  }
+  return results;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+return str.split('')
+.reverse()
+.join('')
 }
 
 module.exports = {
