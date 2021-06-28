a51dc954b339b7a750cc14ec0690a17f18b00bd3
Michael-Fares <mjfares@gmail.com>
2021-06-28 09:16:06 -0500

    node practice init

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,28 @@
 const isEven = (num) => {
   // write code for numbers.isEven
 
+  if(num % 2 === 0) {
+    return true
+  }
+return false
 }
 
 const sum = (arr) => {
-  // write code for numbers.sum
+  return arr.reduce((acc, curr) => acc + curr)
 
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
 
+  for(let i=1; i<arr.length; i++) {
+    for(let j=0; j<i; j++) {
+      if(arr[i] + arr[j] === sum) {
+        return true 
+      }
+    }
+  }
+return falsegit 
 }
 
 module.exports = {
